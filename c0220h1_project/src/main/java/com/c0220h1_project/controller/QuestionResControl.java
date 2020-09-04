b1939393f5571b1e34340a42ae7c2722bf7ae0a3
{chaudang} <{chaudang1595@gmail.com}>
2020-09-04 12:57:48 +0700

    Chau-add questionResController-lan-2

--- c0220h1_project/src/main/java/com/c0220h1_project/controller/QuestionResControl.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/controller/QuestionResControl.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("question")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class QuestionResControl {
 

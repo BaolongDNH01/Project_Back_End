2f2162467d93e27ced34ec468df6130fb1fa26cc
Tinh04 <mytinh04@gmail.com>
2020-09-05 12:01:01 +0700

    update

--- c0220h1_project/src/main/java/com/c0220h1_project/repository/UserRepository.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.c0220h1_project.repository;
 import com.c0220h1_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }

8520bac9a0aa9418dd805cd79dfd1b865edded80
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-06 11:17:00 +0700

    quan update question

--- c0220h1_project/src/main/java/com/c0220h1_project/service/question/impl/QuestionServiceImpl.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/service/question/impl/QuestionServiceImpl.java
@@ -1,23 +1,54 @@
 package com.c0220h1_project.service.question.impl;
 
+import com.c0220h1_project.model.Subject;
 import com.c0220h1_project.model.question.Question;
+import com.c0220h1_project.model.question.QuestionDto;
+import com.c0220h1_project.model.test.Test;
 import com.c0220h1_project.repository.QuestionRepository;
 import com.c0220h1_project.service.question.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class QuestionServiceImpl implements QuestionService {
     @Autowired
     QuestionRepository questionRepository;
+    private QuestionDto convertToQuestionDTO(Question question){
+        QuestionDto questionDto = new QuestionDto();
+        questionDto.setQuestionId(question.getQuestionId());
+        questionDto.setQuestion(question.getQuestion());
+        questionDto.setAnswer(question.getAnswer());
+        questionDto.setRightAnswer(question.getRightAnswer());
+        questionDto.setRightAnswer(question.getRightAnswer());
+        Set<Test> tests = question.getTests();
+        Set<Integer> id = new HashSet<>();
+        for (Test test : tests){
+            id.add(test.getTestId());
+        }
+        questionDto.setTestId(id);
+        Subject subject = question.getSubject();
+        questionDto.setSubjectId(subject.getSubjectId());
+
+        return questionDto;
+    }
     @Override
-    public List<Question> findAll() {
-        return questionRepository.findAll();
+    public List<QuestionDto> findAll() {
+        return ((List<Question>)questionRepository.findAll()).stream().map(this::convertToQuestionDTO).collect(Collectors.toList());
     }
 
     @Override
-    public Question findById(String questionId) {
+    public QuestionDto findById(String questionId) {
+        return (questionRepository.findById(questionId)).map(this::convertToQuestionDTO).orElse(null);
+
+    }
+
+    @Override
+    public Question findByIdQuestion(String questionId) {
         return questionRepository.findById(questionId).orElse(null);
     }
 

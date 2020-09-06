3ebbe751c67bd55692125da260776b9e367aa7ad
chauchau123 <45305889+chauchau123@users.noreply.github.com>
2020-09-06 20:21:26 +0700

    chau-update-code

--- c0220h1_project/src/main/java/com/c0220h1_project/controller/LoginAuthController.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/controller/LoginAuthController.java
@@ -1,31 +1,31 @@
-package com.c0220h1_project.controller;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@CrossOrigin(origins = "*", maxAge = 3600)
-@RestController
-public class LoginAuthController {
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
-    public String adminAccess() {
-        return "This page only for admin !";
-    }
-
-
-    @GetMapping("/member")
-    @PreAuthorize("hasRole('MEMBER')")
-    public String memberAccess() {
-        return "This page only for member (admin still can be enter)";
-    }
-
-    @GetMapping("/common")
-    @PreAuthorize("hasRole('MEMBER') or hasRole('ADMIN')")
-    public String bothRoleAccess() {
-        return "This page only for member and admin";
-    }
-}
+//package com.c0220h1_project.controller;
+//
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@CrossOrigin(origins = "*", maxAge = 3600)
+//@RestController
+//public class LoginAuthController {
+//
+//    @GetMapping("/admin")
+//    @PreAuthorize("hasRole('ADMIN')")
+//    public String adminAccess() {
+//        return "This page only for admin !";
+//    }
+//
+//
+//    @GetMapping("/member")
+//    @PreAuthorize("hasRole('MEMBER')")
+//    public String memberAccess() {
+//        return "This page only for member (admin still can be enter)";
+//    }
+//
+//    @GetMapping("/common")
+//    @PreAuthorize("hasRole('MEMBER') or hasRole('ADMIN')")
+//    public String bothRoleAccess() {
+//        return "This page only for member and admin";
+//    }
+//}
--- c0220h1_project/src/main/java/com/c0220h1_project/model/login_msg/response/JwtResponse.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/model/login_msg/response/JwtResponse.java
@@ -1,5 +1,54 @@
-<<<<<<< HEAD:c0220h1_project/src/main/java/com/c0220h1_project/model/message/response/JwtResponse.java
-//package com.c0220h1_project.model.message.response;
+//<<<<<<< HEAD:c0220h1_project/src/main/java/com/c0220h1_project/model/message/response/JwtResponse.java
+////package com.c0220h1_project.model.message.response;
+////
+////import org.springframework.security.core.GrantedAuthority;
+////
+////import java.util.Collection;
+////
+////public class JwtResponse {
+////
+////    private String token;
+////
+////    private String username;
+////
+////    private Collection<? extends GrantedAuthority> authorities;
+////
+////
+////    public JwtResponse() {
+////    }
+////
+////    public JwtResponse(String token, String username, Collection<? extends GrantedAuthority> authorities) {
+////        this.token = token;
+////        this.username = username;
+////        this.authorities = authorities;
+////    }
+////
+////    public String getToken() {
+////        return token;
+////    }
+////
+////    public void setToken(String token) {
+////        this.token = token;
+////    }
+////
+////    public String getUsername() {
+////        return username;
+////    }
+////
+////    public void setUsername(String username) {
+////        this.username = username;
+////    }
+////
+////    public Collection<? extends GrantedAuthority> getAuthorities() {
+////        return authorities;
+////    }
+////
+////    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+////        this.authorities = authorities;
+////    }
+////}
+//=======
+//package com.c0220h1_project.model.login_msg.response;
 //
 //import org.springframework.security.core.GrantedAuthority;
 //
@@ -47,53 +96,4 @@
 //        this.authorities = authorities;
 //    }
 //}
-=======
-package com.c0220h1_project.model.login_msg.response;
-
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
-
-public class JwtResponse {
-
-    private String token;
-
-    private String username;
-
-    private Collection<? extends GrantedAuthority> authorities;
-
-
-    public JwtResponse() {
-    }
-
-    public JwtResponse(String token, String username, Collection<? extends GrantedAuthority> authorities) {
-        this.token = token;
-        this.username = username;
-        this.authorities = authorities;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
-}
->>>>>>> 783e0c6f98b667289014fed64478e179bf2502a2:c0220h1_project/src/main/java/com/c0220h1_project/model/login_msg/response/JwtResponse.java
+//>>>>>>> 783e0c6f98b667289014fed64478e179bf2502a2:c0220h1_project/src/main/java/com/c0220h1_project/model/login_msg/response/JwtResponse.java

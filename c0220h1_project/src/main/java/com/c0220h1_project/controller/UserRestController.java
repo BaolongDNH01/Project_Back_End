5efd0cca1fc2c21d93ed520660c73e526b367cfb
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-08 12:29:43 +0700

    quan update

--- c0220h1_project/src/main/java/com/c0220h1_project/controller/UserRestController.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/controller/UserRestController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserRestController {
@@ -25,6 +27,10 @@ public class UserRestController {
             return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(userService.findAll(pageable), HttpStatus.OK);
+    }
+    @GetMapping("/allUser")
+    public ResponseEntity<List<User>> getAllUser(){
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/register")

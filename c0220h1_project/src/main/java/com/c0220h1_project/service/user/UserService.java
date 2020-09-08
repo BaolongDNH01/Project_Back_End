e19f3085186d3c5d8fc805401017fb88c25c1339
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-08 12:28:52 +0700

    quan update

--- c0220h1_project/src/main/java/com/c0220h1_project/service/user/UserService.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/service/user/UserService.java
@@ -4,11 +4,14 @@ import com.c0220h1_project.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface UserService {
     boolean save(User user);
     Boolean findByUsername (String Username);
     Page<User> findAll (Pageable pageable);
     void deleteUser (Integer id);
     User findTopByOrderByIdDesc();
+    List<User> findAll();
 
 }

493ea528bd79c73ff28435a33b57ebcd9c364a76
Tinh04 <mytinh04@gmail.com>
2020-09-09 07:58:57 +0700

    tinh - 1st commit - user

--- c0220h1_project/src/main/java/com/c0220h1_project/service/user/impl/UserServiceImpl.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/service/user/impl/UserServiceImpl.java
@@ -1,51 +1,43 @@
 package com.c0220h1_project.service.user.impl;
 
-import com.c0220h1_project.model.Role;
 import com.c0220h1_project.model.User;
-import com.c0220h1_project.model.constant.ERoleName;
-import com.c0220h1_project.repository.RoleRepository;
 import com.c0220h1_project.repository.UserRepository;
 import com.c0220h1_project.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-
 @Service
 public class UserServiceImpl implements UserService {
-
     @Autowired
     UserRepository userRepository;
-    @Autowired
-    RoleRepository roleRepository;
 
-    @Override
-    public boolean save(User user) {
-        if (!findByUsername(user.getUsername())) {
+//    @Override
+    public Boolean save(User user) {
+        if (findByUsername(user.getUsername())) {
             return false;
         }
-        Set<Role> role = new HashSet<>();
-        role.add(roleRepository.findById(2).orElse(null));
-        user.setRoles(role);
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setUser_password(encoder.encode(user.getUser_password()));
         userRepository.save(user);
         return true;
     }
 
+//    @Override
+//    public void save(User user) {
+//        userRepository.save(user);
+//    }
+
     @Override
     public Boolean findByUsername(String Username) {
-        return (userRepository.findByUsername(Username) == null);
+        return userRepository.findAllByUsername(Username) == null;
     }
 
     @Override
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public Page<User> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
     @Override
@@ -59,8 +51,10 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public User findById(Integer id) {
+        return userRepository.findById(id).orElse(null);
     }
+
+
 }
 

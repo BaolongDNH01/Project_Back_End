050a9a5471ada0f39b06e2bc8bc7d2642170c5f5
dautphuong <57697417+dautphuong@users.noreply.github.com>
2020-09-12 10:15:43 +0700

    phuong - update http create

--- c0220h1_project/src/main/java/com/c0220h1_project/controller/ExamController.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/controller/ExamController.java
@@ -3,9 +3,11 @@ package com.c0220h1_project.controller;
 import com.c0220h1_project.model.Exam;
 import com.c0220h1_project.service.exam.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -14,8 +16,11 @@ public class ExamController {
     ExamService examService;
 
     @PostMapping("/create-exam")
-    public void createExam(@RequestBody Exam exam) {
+    public ResponseEntity<Void> createExam(@RequestBody Exam exam, UriComponentsBuilder builder) {
         examService.save(exam);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/get-exam/{id}").buildAndExpand(exam.getExamId()).toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
 

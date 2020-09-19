b240e2312676e214c6324504999387049c933f9e
https://github.com/vongocquan/VoNgocQuan_Module2 <quanv4297@gmail.com>
2020-09-19 19:34:33 +0700

    quan

--- c0220h1_project/src/main/java/com/c0220h1_project/repository/ExamRepository.java
+++ c0220h1_project/src/main/java/com/c0220h1_project/repository/ExamRepository.java
@@ -8,8 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ExamRepository extends JpaRepository<Exam,Integer> {
-    @Query("select u.user.username, sum(u.mark) as point from Exam u group by u.user order by point desc")
-    List<Object> findUserPointDesc();
+    @Query("select u.user.username, count(u.user) as positive from Exam u group by u.user order by positive desc")
+    List<Object> findUserTopPositive();
+
+    @Query("select  u.user.username, sum(u.mark) as point from Exam u where u.test.subject.subjectId = :subjectId group by u.user order by point desc")
+    List<Object> findUserTopExam(Integer subjectId);
 
     //    tinh
     List<Exam> findByUserUsername(String username);
